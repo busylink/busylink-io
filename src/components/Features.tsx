667fbf59fbefc3498ce72a4212ff3b4dfbbbaab8
Jordi Burgos <jordiburgos@gmail.com>
2024-06-27 02:04:59 +0200

    Feature change.

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -8,8 +8,8 @@ export default function Features() {
             <FeatureCard title="Not a boilerplate" description="This is not a boilerplate that you download some code and we forget about you. You get a binary with all the features included. When there is a new release, just download the new version and you get all the new features." />
             <FeatureCard title="Use your preferred language" description="busylink.io is language agnostic. You can have your backed with Node.js (Next.js, Vue, Vite, ...), Go, Rust, Java, Python, even PHP" />
             <FeatureCard title="Working Authentication" description="Authentication working out of the box. User-Password form. Browser Basic authentication, Social with Google, Github, ..." />
-            <FeatureCard title="Admin interface" description="Manage your server with the Admin Interface." />
             <FeatureCard title="User Roles and Permissions" description="Control what your users can access assigning them roles." />
+            <FeatureCard title="Admin interface" description="Manage your server with the Admin Interface. Create users, assign roles, ..." />
         </div>
     </div>
   )
